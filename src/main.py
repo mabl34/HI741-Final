de0067cbe1ebf1ecea2b5bd03ad656b8c5cc1465
Michaela Braun <mk3braun@gmail.com>
2025-05-10 20:29:19 -0500

    Added a test comment to main.py for GitHub commit test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# utilizing user interface for larger range of technical users
 # In[ ]:
 
+# This is a test change to demonstrate a commit
+print("Welcome to the Medical Records CLI")
 
 from ui import launch_ui
 
